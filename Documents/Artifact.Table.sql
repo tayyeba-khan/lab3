949c52e6bbdc54e316d7718f723cc2288976ec54
tayyeba-khan <tmkhan500@gmail.com>
2018-02-26 00:45:28 +0500

    database with access change path

---
+++ Documents/Artifact.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] NCHAR(50) NULL, 
+    [type] NCHAR(10) NULL, 
+    [acquiredProcess] NCHAR(30) NULL, 
+    [Author] NCHAR(50) NULL, 
+    [PublicationDate] DATE NULL, 
+    [genre] NCHAR(10) NULL, 
+    [price] NCHAR(10) NULL, 
+    [batchno] NCHAR(10) NULL, 
+    [quantity] NCHAR(10) NULL
+)
